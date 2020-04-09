75b0e0aeb6401ce885167f2d928ac5b3054f428e
zev see <zevsee@163.com>
2020-04-09 17:18:33 +0800

    understand how stage works

--- hello.c
+++ hello.c
@@ -4,5 +4,6 @@ int main(void)
 	printf("Hello world!\n");
 	printf("this ia a new line \n");
 	printf("this ia a another line.\n");
+	printf("git has a mutable index called stage.\n");
 	return 0;
 }
